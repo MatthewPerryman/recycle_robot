b59eaec4de622a57b29b8ccf795748da99ffbae8
Matt <Matthew_Perryman@outlook.com>
2021-09-25 19:41:06 +0100

    live image returned from api

---
+++ PiCode/rpiWebServer/API/CamStream.py
@@ -0,0 +1,136 @@
+import cv2 #sudo apt-get install python-opencv
+import numpy as np
+import os
+import time
+import keyboard
+from ctypes import *
+
+try:
+    import picamera
+    from picamera.array import PiRGBArray
+except:
+    sys.exit(0)
+   
+# Stock functions except __init__, __del__, and updated take_photo
+class ImageStream():
+	#load arducam shared object file
+	arducam_vcm= CDLL('./lib/libarducam_vcm.so')
+	camera = None
+	resolution = (640, 480, 3)
+	
+	def adjust_lens(self, val):
+		self.arducam_vcm.vcm_write(val)
+		#print("focus value: {}".format(val))
+		
+
+	def laplacian(self, img):
+		img_gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+		img_sobel = cv2.Laplacian(img_gray,cv2.CV_16U)
+		return cv2.mean(img_sobel)[0]
+		
+
+	def calculation(self, camera):
+		rawCapture = PiRGBArray(camera) 
+		self.camera.capture(rawCapture,format="bgr", use_video_port=True)
+		image = rawCapture.array
+		rawCapture.truncate(0)
+		return self.laplacian(image)
+
+	# Runs through all lense focal lengths until clarity reduces, then sets length to best length
+	def focus(self):
+		print("Start focusing")
+		
+		self.max_index = 10
+		self.max_value = 0.0
+		self.last_value = 0.0
+		self.dec_count = 0
+		self.focal_distance = 10
+
+		while True:
+		    #Adjust focus
+		    self.adjust_lens(self.focal_distance)
+		    #Take image and calculate image clarity
+		    val = self.calculation(self.camera)
+		    #Find the maximum image clarity
+		    if val > self.max_value:
+		        self.max_index = self.focal_distance
+		        self.max_value = val
+		        
+		    #If the image clarity starts to decrease
+		    if val < self.last_value:
+		        self.dec_count += 1
+		    else:
+		        self.dec_count = 0
+		    #Image clarity is reduced by six consecutive frames
+		    if self.dec_count > 6:
+		        break
+		    self.last_value = val
+		    
+		    #Increase the focal distance
+		    self.focal_distance += 15
+		    if self.focal_distance > 1000:
+		        break
+
+		#Adjust focus to the best
+		self.adjust_lens(self.max_index)
+
+	# Returns a captured photo
+	# Does not refocus before shot
+	# TODO: (This needs updating when camera motorised in the Z axis!)
+	def take_photo(self):
+		# Create image array
+		image_array = np.empty(self.resolution, dtype=np.uint8)
+		
+		# Catpure image to np array
+		self.camera.capture(image_array, 'rgb')
+		
+		self.camera.close()
+		
+		# Returns image in numpy array format.
+		return image_array
+		
+	def cam_open(self):
+		#open camera
+		self.camera = picamera.PiCamera()
+		
+		#set camera resolution to 640x480(Small resolution for faster speeds.)
+		self.camera.resolution = (self.resolution[1], self.resolution[0])
+		time.sleep(0.1)
+		self.camera.shutter_speed=30000
+
+		# https://picamera.readthedocs.io/en/release-1.13/recipes1.html?highlight=shutter%20speed#capturing-consistent-images
+		# camera.iso = 100
+		# camera.shutter_speed = camera.exposure_speed
+		# camera.exposure_mode = 'off'
+
+		# g = camera.awb_gains
+		# camera.awb_mode = 'off'
+		# camera.awb_gains = g
+		
+		#Adjust focus to the best
+		self.adjust_lens(self.max_index)
+		
+		# Close camera after use in next function
+		
+	def __init__(self):
+		#vcm init
+		self.arducam_vcm.vcm_init()
+		
+		#open camera
+		self.camera = picamera.PiCamera()
+		
+		#set camera resolution to 640x480(Small resolution for faster speeds.)
+		self.camera.resolution = (self.resolution[1], self.resolution[0])
+		time.sleep(0.1)
+		self.camera.shutter_speed=30000
+		
+		# Determine focus values
+		# Using this class in a Flask app runs init twice therefore self.camera.close() is needed
+		self.focus()
+		
+		# Clear up
+		self.camera.close()
+		
+
+	def __del__(self):
+		self.camera.close()
--- PiCode/rpiWebServer/API/api.py
+++ PiCode/rpiWebServer/API/api.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask import send_from_directory
 import cv2
+from CamStream import ImageStream
+import numpy as np
 
 app = Flask(__name__)
+image_stream = ImageStream()
 
 @app.route('/', methods=['GET'])
-def image():
-	return send_from_directory('', 'img26.jpg')
+def live_photo():
+	image_stream.cam_open()
+	image = image_stream.take_photo()
+	return image.tobytes()
 	
 if __name__ == '__main__':
 	app.run(debug=True, port=80, host='0.0.0.0')
