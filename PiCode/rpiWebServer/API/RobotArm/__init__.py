2715b33a41affd3c788d16d659d58a33c4e73dde
Matt <Matthew_Perryman@outlook.com>
2021-10-09 12:20:14 +0100

    Creation of RobotController Class and added move function to API

---
+++ PiCode/rpiWebServer/API/RobotArm/__init__.py
@@ -0,0 +1,1 @@
+from . import ArmPositionSetter, RobotController
--- PiCode/rpiWebServer/API/api.py
+++ PiCode/rpiWebServer/API/api.py
@@ -3,12 +3,23 @@ from flask import send_from_directory
 import cv2
 from CamStream import ImageStream
 import numpy as np
+from RobotArm import RobotController
 
 app = Flask(__name__)
-# The camera is focussed hear, therefore set up lighting before starting the app
+# The camera is focussed here, therefore set up lighting before starting the app
 image_stream = ImageStream()
+controller = RobotController.RobotController()
 
-@app.route('/', methods=['GET'])
+@app.route('/update_position', methods=['PUT'])
+def increment_position():
+	update_vector = request.content
+	if (update_vector > -10) and (update_vector < 10):
+		controller.move_by_increment(update_vector)
+		return "API Check Success"
+	else:
+		return "API Check Fail"
+
+@app.route('/get_photo', methods=['GET'])
 def live_photo():
 	image_stream.cam_open()
 	image = image_stream.take_photo()
