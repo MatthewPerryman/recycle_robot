6456c6990b343da374dcd55a89c81a003afb8a13
Matthew Perryman <matthew_perryman@outlook.com>
2021-11-06 16:02:28 +0000

    Better defining opening and closing steps of robot comms

--- PiCode/rpiWebServer/API/RobotArm/RobotController.py
+++ PiCode/rpiWebServer/API/RobotArm/RobotController.py
@@ -2,13 +2,21 @@ from uarm.wrapper import SwiftAPI
 
 
 class RobotController:
+	swift = None
+	
+	def start_transmission(self):
+		self.swift.flush_cmd(wait_stop=True)
+		self.swift.waiting_ready()
 
 	## Clear cmd buffer and close connection
 	def end_transmission(self):
 		self.swift.flush_cmd(wait_stop=True)
+		self.swift.waiting_ready()
 
 	## Move the robot arm by an increment value
 	def move_by_vector(self, vector, reverse_vector=False, speed=100000):
+		self.start_transmission()
+
 		# Calculate new position and send update
 		# TODO: Create vector class
 		old = self.swift.get_position()
@@ -19,18 +27,26 @@ class RobotController:
 			new_location = [old[0] + vector[0], old[1] + vector[1], old[2] + vector[2]]
 
 		self.move_to(new_location)
+		
+		self.end_transmission()
 
 		return old, new_location
 
 	## Move the robot arm to this vector
 	# TODO: Return boolean of success
 	def move_to(self, new_location, speed=100000):
+		self.start_transmission()
+
 		self.swift.set_position(x=new_location[0], y=new_location[1], z=new_location[2])
+
+		self.end_transmission()
 		return new_location
 
 	## Create the API context and put robot on standby
 	def __init__(self):
-		self.swift = SwiftAPI()
+		if self.swift == None:
+			self.swift = SwiftAPI()
+
 		self.swift.flush_cmd(wait_stop=True)
 
 		# Reset the arms location
